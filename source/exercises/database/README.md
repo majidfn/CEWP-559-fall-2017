f8eb96887f25125981f5680cd90dc43bcdfb6100
Majid Fatemian <majidfn@yahoo.com>
2017-09-28 12:39:22 -0400

    Databases exercises

---
+++ source/exercises/database/README.md
@@ -0,0 +1,14 @@
+# Database #
+
+## MySQLi Procedural ##
+
+### Exercise - 1 ###
+
+Database currently has more than 1 record. Show all of them
+
+### Exercise - 2 ###
+
+Show the price of each Book following the below format. Keep the whole formatter string in double-quotes:
+```
+ID NAME ($PRICE) 
+```
--- source/exercises/database/mysqli_procedural.php
+++ source/exercises/database/mysqli_procedural.php
@@ -1,13 +1,4 @@
 <?php
-
-//
-// Exercise - 1:
-// Database currently has more than 1 record. Show all of them
-// 
-// Exercise - 2:
-// Show the price of each Book following the below format. Keep the whole formatter string in double-quotes.
-//  ID NAME ($PRICE) 
-
 $conn = mysqli_connect('mysql', 'root', 'root', 'CCE_PHPMySQL2');
 
 if (!$conn) {
