fcfd1b3224e0ea870fc652eb5a8ee9c57ea6459a
Majid Fatemian <majidfn@yahoo.com>
2017-09-28 16:00:56 -0400

    Project Skeleton

---
+++ source/application/views/item_view.php
@@ -0,0 +1,16 @@
+<?php
+
+class ItemView
+{
+    private $model;
+
+    public function __construct($model) {
+        $this->model = $model;
+    }
+	
+    public function output(){
+        $data = $this->model->data;
+         
+        return json_encode($data, JSON_PRETTY_PRINT);
+    }
+}
