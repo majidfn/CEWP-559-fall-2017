42dc5961ad7c95c6b9214a408d177d5452a5963c
Majid Fatemian <majidfn@yahoo.com>
2017-10-22 23:57:47 -0400

    Cleaning up the Loader. Now we are using AutoLoader

--- source/api/loader.php
+++ source/api/loader.php
@@ -1,24 +1,12 @@
 <?php
 
-require_once __DIR__.'/models/base_model.php';
+spl_autoload_register(function ($class_name) {
 
-// Item
-require_once __DIR__.'/models/item_model.php';
-require_once __DIR__.'/views/item_view.php';
-require_once __DIR__.'/controllers/item_controller.php';
+    preg_match_all('/[A-Z][a-z]+/', $class_name, $matches);
 
-// Category
-require_once __DIR__.'/models/category_model.php';
-require_once __DIR__.'/controllers/category_controller.php';
+    $paths = $matches[0];
+    $paths = array_values(array_filter($paths));
+    $paths = array_map('strtolower', $paths);
 
-
-// spl_autoload_register(function ($class_name) {
-
-//     preg_match_all('/[A-Z][a-z]+/', $class_name, $matches);
-
-//     $paths = $matches[0];
-//     $paths = array_values(array_filter($paths));
-//     $paths = array_map('strtolower', $paths);
-
-//     include __dir__."/{$paths[1]}s/{$paths[0]}_{$paths[1]}.php";
-// });
+    include __dir__."/{$paths[1]}s/{$paths[0]}_{$paths[1]}.php";
+});
